094aafe9fbf0eb1dda187333f26365f2345ee2b9
Isaac Quinzada <iqfast13@gmail.com>
2018-11-06 15:08:45 -0500

    :apple:
    update splash screen

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function (){
+    // Carga de la pagina
+    var preload = document.getElementById("preload");
+    var loading = 0;
+    var id = setInterval(frame, 64);
+
+    function frame(){
+        if (loading==50){
+            clearInterval(id)
+            window.open("https://altkemist.github.io/altkemist", "_self");
+        }
+        else {
+            loading += 1;
+            if (loading == 40){
+                preload.style.animation = "fradeout 1s ease"
+            }
+        }
+    }
+})();
